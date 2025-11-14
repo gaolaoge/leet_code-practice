db9cd5434d91c4c9da7b8a5bd20ed906a105f2ba
gaolaoge <gaolaoge@sina.cn>
2025-11-14 18:34:18 +0800

    日常提交

---
+++ record/20251114-数据结构/1.LRU 缓存.ts
@@ -0,0 +1,25 @@
+/*
+ * [146] LRU 缓存
+ *
+ */
+
+class LRUCache {
+  constructor(capacity: number) {}
+
+  get(key: number): number {}
+
+  put(key: number, value: number): void {}
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+/**
+ * LRU 是一种常用的缓存淘汰策略，当缓存满时，优先淘汰最久未使用的数据。
+ */
+
+export {};
