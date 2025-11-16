48519492ded27989137f2274983461dc1927bde1
gaolaoge <gaolaoge@sina.cn>
2025-11-16 09:25:52 +0800

    日常提交

--- record/20251114-数据结构/1.LRU 缓存.ts
+++ record/20251114-数据结构/1.LRU 缓存.ts
@@ -1,14 +1,73 @@
 /*
  * [146] LRU 缓存
- *
+ * https://leetcode.cn/problems/lru-cache/
  */
 
+class IListNode {
+  key: number;
+  val: number;
+  prev?: IListNode;
+  next?: IListNode;
+  constructor(key: number, val: number) {
+    this.key = key;
+    this.val = val;
+  }
+}
+
 class LRUCache {
-  constructor(capacity: number) {}
+  capacity: number;
+  map: Map<number, IListNode>;
+  head: IListNode;
+  tail: IListNode;
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.map = new Map();
 
-  get(key: number): number {}
+    this.head = new IListNode(0, 0);
+    this.tail = new IListNode(0, 0);
 
-  put(key: number, value: number): void {}
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  get(key: number): number {
+    const node = this.map.get(key);
+    if (!node) return -1;
+    this.removeNode(node);
+    this.addNodeToHead(node);
+    return node.val;
+  }
+
+  put(key: number, value: number): void {
+    const node = this.map.get(key);
+    if (node) {
+      node.val = value;
+      this.removeNode(node);
+      this.addNodeToHead(node);
+    } else {
+      if (this.map.size >= this.capacity) {
+        const lastNode = this.tail.prev!;
+        this.removeNode(lastNode);
+        this.map.delete(lastNode.key);
+      }
+      const newNode = new IListNode(key, value);
+      this.map.set(key, newNode);
+      this.addNodeToHead(newNode);
+    }
+  }
+
+  removeNode(node: IListNode): void {
+    node.prev!.next = node.next;
+    node.next!.prev = node.prev;
+  }
+
+  addNodeToHead(node: IListNode): void {
+    node.prev = this.head;
+    node.next = this.head.next;
+
+    node.next!.prev = node;
+    this.head.next = node;
+  }
 }
 
 /**
@@ -20,6 +79,13 @@ class LRUCache {
 
 /**
  * LRU 是一种常用的缓存淘汰策略，当缓存满时，优先淘汰最久未使用的数据。
+ * 核心原则：
+ * 1. 最近使用的数据更有价值：刚被访问的数据在未来被再次访问的概率更高
+ * 2. 淘汰最久未使用的数据：长时间未被访问的数据被认为重要性较低
+ */
+
+/**
+ * 哈希 + 双链表：实现O(1)时间复杂度的LRU缓存。
  */
 
 export {};
---
+++ record/20251114-数据结构/2.LFU 缓存.ts
@@ -0,0 +1,25 @@
+/**
+ * [460] LFU 缓存
+ * https://leetcode.cn/problems/lfu-cache/description/
+ */
+
+class LFUCache {
+  constructor(capacity: number) {}
+
+  get(key: number): number {}
+
+  put(key: number, value: number): void {}
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * var obj = new LFUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+/**
+ * LFU (最不经常使用)：只关心访问时间，最近访问的放前面。
+ */
+
+export {};
