c36f7da70bf6d7dc728a4e311b4c2aedc357fe50
gaolaoge <gaolaoge@sina.cn>
2025-11-18 00:22:03 +0800

    fix: -

---
+++ record/20251114-数据结构/3.我的日程安排表 I.ts
@@ -0,0 +1,22 @@
+/*
+ * [729] 我的日程安排表 I
+ * https://leetcode.cn/problems/my-calendar-i/description/
+ */
+
+class MyCalendar {
+  constructor() {}
+
+  book(startTime: number, endTime: number): boolean {}
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(startTime,endTime)
+ */
+
+/**
+ *
+ */
+
+export {};
